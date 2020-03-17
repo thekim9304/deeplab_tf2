a87f3dbaf60fd6039e8f3cf7fb1dcec6fe169ea6
kimtae <thek9304@gmail.com>
2020-03-17 21:05:32 +0900

    1

---
+++ utils/load_data.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+def load_mnist():
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    # 채널 차원을 추가합니다.
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+
+    print('x_train.shape : {}, x_test.shape : {}'.format(x_train.shape, x_test.shape))
+
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(10000).batch(32)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+    # return train_ds, test_ds
+    return x_train, y_train
